68d74c72ccf087ee3b4a47d88a25a32bf2de6771
Ariunbold <ariun1118@gmail.com>
2020-08-13 21:48:44 -0400

    Day 2 MVP done

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,24 @@
 from room import Room
 from player import Player
+from item import Item
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons", [Item("Shield", "Gives you an Armor.")]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""",  [Item("Sword", "Arthur sword")]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",  [Item("Helmet", "A long lived hero's helmet")]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""",  [Item("Bow", "Mongolian bow")]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",  [Item("Pot of Gold", "The treasure that you have been looking for..")]),
 }
 
 
@@ -38,19 +38,22 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-new_player = Player("Andy", room['outside'])
+player_name = input("What is your name?\n")
+new_player = Player(player_name, room['outside'])
 
 directions = ["n", "s", "e", "w", "q"]
+
 
 def commands():
     for command in directions.items():
         print(f"{command}")
 
+
 print(f"Welcome, {new_player.name}")
-print("Command: [q] exit the game, [n,s,e,w] to move around ")
+print("Command: [q] exit the game, [n,s,e,w] to move around and [g] to grab the item, [d] to drop the item ")
 
 # Write a loop that:
-#
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
@@ -67,5 +70,22 @@ while True:
     elif cmd in directions:
         new_player.move(cmd)
         print(f"\nYou are in: The {new_player.currentRoom}")
+
+    elif cmd == 'g':
+        if len(new_player.currentRoom.items) == 0:
+            cmd = input(
+                'There are no more items to pick up. Please make your next move:\n')
+        else:
+            new_player.pickup_item(new_player.currentRoom.items[0])
+            new_player.currentRoom.remove_item(new_player.currentRoom.items[0])
+    elif cmd == 'd':
+        if len(new_player.inventory) < 1:
+            cmd = input(
+                'You have no items to drop. Please make your next move:\n')
+        else:
+            new_player.currentRoom.add_item(new_player.inventory[0])
+            new_player.drop_item(new_player.inventory[0])
+    elif cmd == 'r':
+        new_player.inventory
     else:
         print("Invalid Input")
--- src/player.py
+++ src/player.py
@@ -5,9 +5,7 @@ class Player:
     def __init__(self, name, currentRoom):
         self.name = name
         self.currentRoom = currentRoom
-
-    # def __str__(self):
-    #     return f"{self.name}\n\n {self.currentRoom}"
+        self.inventory = []
 
     def move(self, direction):
         next_room = getattr(self.currentRoom, f"{direction}_to")
@@ -15,3 +13,17 @@ class Player:
             self.currentRoom = next_room
         else:
             print("\nNot Allowed to go that way")
+
+    def pickup_item(self, item):
+        self.inventory.append(item)
+        print(f'You picked up the {item.name}\n')
+
+    def drop_item(self, item):
+        self.inventory.remove(item)
+        print(f'You dropped the {item.name}\n')
+
+    def __str__(self):
+        inventory_items= ''
+        for item in self.inventory:
+            inventory_items += f'\n{item}'
+        return f"Your inventory items: {self.inventory_items}"
